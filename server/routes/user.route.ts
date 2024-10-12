333db5c8ddb14248fd91780f8e3a02d653532da2
Aryan Hedaoo <aryanhedaoo@Aryans-MacBook-Air.local>
2024-10-12 19:49:17 +0530

    first commit

---
+++ server/routes/user.route.ts
@@ -0,0 +1,12 @@
+import  express  from "express";
+import { activateUser, loginUser, registrationUser } from "../controllers/user.controller";
+import exp from "constants";
+const userRouter =express.Router();
+
+userRouter.post('/registration',registrationUser);
+
+userRouter.post('/activate-user',activateUser);
+
+userRouter.post('/login',loginUser);
+
+export default userRouter;
