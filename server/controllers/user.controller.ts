7b13fe3d8271d11aa2bb86f8b02fa973e48d18ec
Aryan Hedaoo <aryanhedaoo@Aryans-MacBook-Air.local>
2024-10-13 18:32:48 +0530

    authorization

--- server/controllers/user.controller.ts
+++ server/controllers/user.controller.ts
@@ -8,6 +8,9 @@ import ejs from "ejs";
 import path from "path";
 import sendMail from "../utils/sendMail";
 import { sendToken } from "../utils/jwt";
+import { redis } from "../utils/redis";
+
+
 
 //register user
 interface IRegistrationBody {
@@ -164,6 +167,9 @@ export const logoutUser = CatchAsyncError(
     try {
       res.cookie("access_token", "", { maxAge: 1 });
       res.cookie("refresh_token", "", { maxAge: 1 });
+      const userId = String(req.user?._id || "");
+
+      redis.del(userId)
       res.status(200).json({
         success: true,
         message: "Logged out successfully",
--- server/routes/user.route.ts
+++ server/routes/user.route.ts
@@ -1,6 +1,6 @@
 import  express  from "express";
 import { activateUser, loginUser, logoutUser, registrationUser } from "../controllers/user.controller";
-import exp from "constants";
+import { authorizeRoles, isAutheticated } from "../middleware/auth";
 const userRouter =express.Router();
 
 userRouter.post('/registration',registrationUser);
@@ -9,6 +9,8 @@ userRouter.post('/activate-user',activateUser);
 
 userRouter.post('/login',loginUser);
 
-userRouter.get('/logout',logoutUser);
+userRouter.get('/logout',isAutheticated,authorizeRoles("admin"),logoutUser);
+
+
 
 export default userRouter;
